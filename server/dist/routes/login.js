c3aa262ef9833300a5dca49812cad1a8aa230451
Richino Archer <archerrichin@gmail.com>
2023-02-07 08:39:21 -0500

    changes

--- server/dist/routes/login.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.post("/login", (req, res) => {
-    const { username, password } = req.body;
-});
-//# sourceMappingURL=login.js.map
