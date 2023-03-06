52d7a3814b5b744fa7d250474ba0c690f6538aec
Richino Archer <archerrichin@gmail.com>
2023-03-06 14:23:12 -0500

    fixed issues with stories has no height

--- client/components/common/avatar.tsx
+++ client/components/common/avatar.tsx
@@ -1,25 +1,27 @@
+import Image from "next/image";
+
 interface Props {
 	image: string;
-	height: number;
+	height?: number | 60;
 	width: number;
 	story: boolean;
 }
 export default function Avatar(props: Props) {
 	return (
 		<div
-			className={`shrink-0 overflow-hidden  rounded-full hover:cursor-pointer h-[${
-				props.height
-			}px] w-[${props.width}px]  ${
-				props.story &&
-				"bg-gradient-to-r from-indigo-500 via-purple-500 to-fuchsia-500 p-[2px]"
-			} `}>
-			<img
-				src={props.image}
-				className={`h-full w-full rounded-full object-cover ${
-					props.story ? "bg-white p-[2px]" : null
-				}`}
-				alt="avatar"
-			/>
+			className={`relative rounded-full ${
+				props.story && "bg-gradient-to-r from-indigo-500 via-purple-500 to-fuchsia-500 p-[2px]"
+			}`}>
+			<div
+				className={`relative shrink-0  overflow-hidden min-h-[${props.height}px]  rounded-full border-[2px] border-white hover:cursor-pointer h-[${props.height}px] w-[${props.width}px] `}>
+				<Image
+					src={props.image}
+					alt="avatar"
+					fill
+					style={{ objectFit: "cover" }}
+					sizes="(max-width: 56px) 100vw"
+				/>
+			</div>
 		</div>
 	);
 }
