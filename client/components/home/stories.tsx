de0e08a461ef9b7bbc34e72e856550422a83a964
Richino Archer <archerrichin@gmail.com>
2023-03-06 14:23:38 -0500

    fix issues where stories has no height

--- client/components/home/stories.tsx
+++ client/components/home/stories.tsx
@@ -7,19 +7,15 @@ export default function Stories() {
 	const [data, setData] = useState(stories);
 
 	return (
-		<div className="relative  h-max  w-full  rounded-lg  bg-white tablet:phone:rounded-none">
+		<div className=" h-max  w-full  rounded-lg  bg-white tablet:phone:rounded-none">
 			<div className="stories flex gap-4 overflow-y-auto p-5 pb-2">
 				{data.map((key, index) => {
 					return (
 						<div
 							key={index}
-							className="flex w-[90px] max-w-[90px]  flex-col items-center justify-center gap-1 ">
-							<div className="relative h-[65px] w-[65px]">
-								<Avatar height={65} width={65} story={true} image={key.avatar} />
-							</div>
-							<span className=" grid w-[90px] max-w-[90px] place-items-center truncate ">
-								{key.fullname}
-							</span>
+							className="flex w-[60px] min-w-[60px] flex-col items-center justify-center gap-1   ">
+							<Avatar height={60} width={60} story={true} image={key.avatar} />
+							<span className=" w-[60px] min-w-[60px] truncate">{key.fullname}</span>
 						</div>
 					);
 				})}
